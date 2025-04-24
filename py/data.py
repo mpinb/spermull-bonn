2efd6973eb247a88f00c98885744874f10960257
Bjorge Meulemeester <bjorge.meulemeester@mpinb.de>
2025-04-24 14:35:46 +0200

    fetch lat and lon from date

---
+++ py/data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from isf_pandas_msgpack import read_msgpack
+import datetime, os
+
+wd = os.path.dirname(__file__)
+DATA_DIR = os.path.abspath(os.path.join(wd, "..", "data"))
+DF_PATH = os.path.join(DATA_DIR, "geo_dates.msg")
+
+def read_df():
+    return read_msgpack(DF_PATH)
+
+def get_lat_lon_from_date(
+    date: str
+    ):
+    """Get geolocation from a datetime"""
+    return read_df().xs(date, level='date')[['LAT', 'LON']].values
